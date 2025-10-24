dabb6505d259f78a8f65e9e74c6bf0a527059cfb
abel2606 <150111881+abel2606@users.noreply.github.com>
2025-10-23 17:14:33 -0700

    first commit

---
+++ MODELOS/scrpt.js
@@ -0,0 +1,49 @@
+const marcasSelect = document.getElementById('marcas');
+const selectContainer = document.getElementById('select-container');
+const modeloSelect = document.getElementById('modelos');
+
+fetch('http://localhost:8888/marcas')
+.then(response => response.text())
+.then(data =>{
+    const marcas = data.split('\n');
+    marcas.forEach(marca =>{
+        const option = document.createElement('option');
+        option.value = marca;
+        option.textContent = marca;
+        marcasSelect.appendChild(option);
+
+    })
+
+    marcasSelect.addEventListener('change', ()=>{
+        const selectedMarca =marcasSelect.value;
+        cargarModelos(selectedMarca);
+    })
+
+
+})
+
+function cargarModelos(marca){
+    fetch(`http://localhost:8888/marcas/modelos/${marca}`)
+    .then(response => response.text())
+    .then(data =>{
+        const modelos = data.split('\n');
+        modeloSelect.innerHTML = '<option value="">Seleccione un modelo</option>';
+        modelos.forEach(modelo=>{
+            const option= document.createElement('option');
+            option.value = modelo;
+            option.textContent = modelo;
+
+            const marcaSinEspacio = marca.trim();
+            option.classList.add(`is-${marca}`)
+        
+            modeloSelect.appendChild(option)
+        })
+    })
+}
+
+modeloSelect.addEventListener('change', ()=>{
+    const modelo = modeloSelect.value;
+    if(modelo){
+        alert(`Modelo seleccionado: ${modelo}`)
+    }
+})
